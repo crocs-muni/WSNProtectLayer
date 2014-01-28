eb8040cb8afafd6f98f20274bb81bc30df88f985
ph4r05 <ph4r05@gmail.com>
2014-01-28 20:09:13 +0100

    Printf shell scripts fixes, reset nodes before start option

--- ManagementApp/Daemon/runPrintf.sh
+++ ManagementApp/Daemon/runPrintf.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
+#
+# This script starts Printf clients for all nodes defined in nodelist.
+#
+# @Author: Ph4r05
+#
 
 fname=$1
 logdir=$2
-if [[ "x$fname" == "x"  || "x$logdir" == "x" ]]; then
-	echo "Usage: $0 nodelist"
+reset=$3
+
+# Some sanity checks
+if [[ $# -lt 2 || "x$fname" == "x"  || "x$logdir" == "x" ]]
+then
+	echo "Usage: $0 nodelist logdir [reset-node]"
+	echo ""
+	echo "   nodelist     List of nodes to listen. One node alias"
+	echo "                (e.g. /dev/mote_telos40) per line"
+	echo "   logdir       Directory where to place log files"
+	echo "   reset        Resets node just before PrintfClient is started"
+	echo "                Warning: assumes all connected nodes are TelosB!"
+	echo ""
 	exit 1
 fi
 
 if [ ! -d "$logdir" ]; then
 	echo "Logdir $logdir does not exist! I'll try to create it for you dude..."
-	mkdir "$logdir" || echo "Sorry, it is not possible..."; exit 2
+	mkdir "$logdir" || echo "Sorry, it is not possible..." || exit 2
 fi
 
-nodelist=`cat $fname`
-echo $nodelist
+if [ "x$reset" == "x" ]; then
+	reset=0
+fi
 
+RCol='\e[0m'   
+Red='\e[0;31m';
+Gre='\e[0;32m'; 
+UBlu='\e[4;34m';
+
+# Main code
+nodelist=`cat $fname`
 for node in $nodelist; do
 	nodefile="$logdir/`basename $node`"
-	echo "current node: $node file $nodefile"
-	java net.tinyos.tools.PrintfClient -comm "serial@$node:telosb" 2> $nodefile 1> $nodefile &
+	echo ""
+	echo -e "Current node: ${UBlu}$node${RCol} file: $nodefile"
+	
+	if [ $reset -eq 1 ]; then
+		echo -n "Going to reboot node $node                   "
+		`which tos-bsl` --telosb -r -c $node &> /dev/null
+		RT=$?
+		if [ ! $RT -eq 0 ]; then
+			echo -e "${Red}Error: ${RCol}Node restart failed"
+		else
+			echo -e "${Gre}[  OK  ]${RCol}"
+		fi
+	fi
+
+	echo -n "Going to start PrinfClient on node $node     "	
+	java net.tinyos.tools.PrintfClient -comm "serial@$node:telosb" &> $nodefile &
+	echo -e "${Gre}[  OK  ]${RCol}"
 done
 
